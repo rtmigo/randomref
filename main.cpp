b10b1260191a5b026244dc48d6ceb263b5725ccd
rtmigo <art.galkin@gmail.com>
2021-03-26 03:04:49 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <list>
+#include <vector>
+#include <string>
 #include <math.h>
 
 using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////	
+// "in C99 a 64-bit unsigned integer x should be converted to a 64-bit 
+// double using the expression"
+// by Sebastiano Vigna <https://prng.di.unimi.it/>
+
+static double vigna_uint64_to_double_mult(uint64_t x) {
+	return (x >> 11) * 0x1.0p-53;
+}
+
+// "An alternative, multiplication-free conversion" suggestion 
+// by Sebastiano Vigna <https://prng.di.unimi.it/>
+static inline double vigna_uint64_to_double_alt(uint64_t x) {
+	const union { uint64_t i; double d; } u = { 
+		.i = UINT64_C(0x3FF) << 52 | x >> 12 
+	};
+	return u.d - 1.0;
+}
 
 ////////////////////////////////////////////////////////////////////////////////	
 // Jurgen A. Doornik. 2007. Conversion of high-period random numbers to 
@@ -29,13 +49,18 @@ using namespace std;
 class Int32Generator {
     public:
         virtual uint32_t next() = 0;
+        string seed_str, algo;
 };
 
 class Xorshift32: public Int32Generator {
 public:
     Xorshift32(uint32_t a) {
         this->a = a;
+        this->seed_str = to_string(a);
+        this->algo = "xorshift32";
     }
+
+    //uint32_t seedA;
 
     uint32_t a;    
     
@@ -51,42 +76,73 @@ public:
 
 template <typename T>
 
-void print_list(list<T> &values, char* format) {
+void print_list(vector<T> &values, string fmt) {
     bool isFirst=true;
     for (auto const& x : values) {
-        printf(isFirst ? "\t" : ",\n\t");
+        cout << (isFirst ? "\t" : ",\n\t");
         isFirst = false;
         char valueStr[256];
-        sprintf(valueStr, format, x);
-        printf("\"%s\"", valueStr);	
+        sprintf(valueStr, fmt.c_str(), x);
+        cout << '"' << valueStr << '"';
+        //printf("\"%s\"", valueStr);	
     }    
 }
 
-void print32(Int32Generator* generator) {
-    list<uint32_t> ints;
-    list<double_t> randbl32;
+void print32(Int32Generator& generator, string id) {
+    vector<uint32_t> ints;
+    vector<double_t> randbl32;
 
-    for (int i=0; i<10; ++i) {
-        int32_t x = generator->next();
+    for (uint32_t i=0; i<10; ++i) {
+        int32_t x = generator.next();
         ints.push_back(x);
         randbl32.push_back(RANDBL_32(x));
     }
-    cout << "\"ints\": [\n";
+
+    vector<double_t> doubles_mult, doubles_alt;
+
+    for (int32_t i=0; i<ints.size(); i+=2) {
+        uint64_t combined = (((uint64_t)ints[i])<<32)|ints[i+1];
+        doubles_mult.push_back(vigna_uint64_to_double_mult(combined));
+        doubles_alt.push_back(vigna_uint64_to_double_alt(combined));
+    }
+
+    cout << "{\n";
+
+    cout << "\"algorithm\": " << '"' << generator.algo << "\"," << endl;
+    cout << "\"sample_id\": " << '"' << id << "\"," << endl;
+    cout << "\"comment\": " << "\"seed " << generator.seed_str << "\"," << endl;
+
+    cout << "\"uint32\": [\n";
     print_list<uint32_t>(ints, "%08x");
-    cout << "\n],";
+    cout << " ],";
     cout << endl;
 
-    cout << "\"randbl32\": [\n";
+    cout << "\"double_doornik_randbl32\": [\n";
     print_list<double_t>(randbl32, "%.20e");
-    cout << "\n],";    
+    cout << " ],\n";    
+
+    cout << "\"double_vigna_multiplication\": [\n";
+    print_list<double_t>(doubles_mult, "%.20e");
+    cout << " ],\n";    
+
+    cout << "\"double_vigna_memcast\": [\n";
+    print_list<double_t>(doubles_alt, "%.20e");
+    cout << " ]\n";    
+
+    cout << "}";
 }
 
 int main() {
 
-    Xorshift32 xorsh(1);
+	#define PI32 314159265
+	#define PI64 3141592653589793238ll    
 
-    print32(&xorsh);
-    cout << endl;
+    Xorshift32 xorshA(1), xorshB(42), xorshC(PI32);
+
+    print32(xorshA, "a");    cout << ",\n\n";
+    print32(xorshB, "b");    cout << ",\n\n";
+    print32(xorshC, "c");    cout << endl;
+
     //cout << "hi!";
     //cout << xorsh.next() << " " << xorsh.next();
     //cout << endl;
